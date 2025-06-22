6b5c4be2e1cbe4443e8eafde40a14cf44b3a0a86
MxSns <maxime.sn210@gmail.com>
2025-06-22 23:40:47 +0200

    add title

--- web_crawler.py
+++ web_crawler.py
@@ -1,8 +1,13 @@
 # Script : web_crawler.py
 # Author : mxn
 
-import argparse, requests, sys, re
+import argparse, requests, sys, re, shutil
+from rich.console import Console
 from urllib.parse import urljoin
+
+console = Console()
+
+width = shutil.get_terminal_size().columns
 
 parser = argparse.ArgumentParser(description='Web crawler script to recursively visit pages within a specified domain, logging crawled URLs and external references.\nRun with: python3 web_crawler.py --domain <URL>')
 parser.add_argument('-d', '--domain', required=True, help='the domain to crawl in')
@@ -15,6 +20,7 @@ external = []
 
 # Request a webpage and return the html text
 def fetch_page(url):
+    
     try:
         response = requests.get(url)
 
@@ -34,7 +40,9 @@ def get_linked_pages(html):
     pattern = r'<a\s+href=\"?([^\">\s]+).*?([a-z0-9: ]+)</a>'
 
     links = re.findall(pattern, html, re.I)
+        
     for link in links:
+
         # Access the URL in match group 0.
         this_url = link[0]
         this_url = this_url.split("#")[0]
@@ -48,6 +56,11 @@ def get_linked_pages(html):
         
         elif page not in external:
              external.append(page)
+        
+print("=" * width + "\n")
+console.print("WEB-CRAWLER", justify="center")
+print("\n" + "=" * width )
+print(f"\nEnumerating links form {args.domain} ...\n")
 
 # Loopin to_crawl until list is empty
 while to_crawl:
@@ -56,4 +69,7 @@ while to_crawl:
         get_linked_pages(html)
 
 print("\nCrawled URLs:\n" + "\n".join(crawled))
-print("\nExternal URLs:\n" + "\n".join(external))
+if not external:
+    print(f"No external links were found in {dmn}")
+else:
+    print("\nExternal URLs:\n" + "\n".join(external))
